3c918f7878227a250687a0aeec9c58476d6b4593
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-09 16:57:29 +0000

    fix: fix grant statement

--- src/Limax.php
+++ src/Limax.php
@@ -6,15 +6,18 @@ use Illuminate\Support\Facades\DB;
 
 class Limax
 {
-  public function getSchema() {
+  public function getSchema()
+  {
     return 'limax';
   }
 
-  public function grant(string $role) {
+  public function grant(string $role)
+  {
     collect([
-          'GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s;',
-          'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1\$s.slugs TO %2\$s;'
-        ])->each(fn(string $statement) => DB::statement(sprintf($statement, Limax::getSchema(), $role)));
+      'GRANT USAGE ON SCHEMA %1\$s TO %2\$s',
+      'GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s;',
+      'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1\$s.slugs TO %2\$s;'
+    ])->each(fn (string $statement) => DB::statement(sprintf($statement, Limax::getSchema(), $role)));
   }
 
   public function ungrant(string $role)
