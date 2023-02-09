a38742a77dfe7b9a980f3c02c4ab327f4f1bf231
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-09 16:56:03 +0000

    feat: add ungrant method

--- src/Limax.php
+++ src/Limax.php
@@ -11,26 +11,13 @@ class Limax
   }
 
   public function grant(string $role) {
-    DB::statement(
-      sprintf(
-        <<<PLPGSQL
-GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s;
-PLPGSQL
-        ,
-        Limax::getSchema(),
-        $role
-      )
-    );
-    
-    DB::statement(
-      sprintf(
-        <<<PLPGSQL
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1\$s.slugs TO %2\$s;
-PLPGSQL
-        ,
-        Limax::getSchema(),
-        $role
-      )
-    );
+    collect([
+          'GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s;',
+          'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1\$s.slugs TO %2\$s;'
+        ])->each(fn(string $statement) => DB::statement(sprintf($statement, Limax::getSchema(), $role)));
+  }
+
+  public function ungrant(string $role)
+  {
   }
 }
