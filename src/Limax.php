425b6a94e51b1470933c01dd7fe8875128ac16f6
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-09 17:04:05 +0000

    fix: fix migration

--- src/Limax.php
+++ src/Limax.php
@@ -14,9 +14,9 @@ class Limax
   public function grant(string $role)
   {
     collect([
-      'GRANT USAGE ON SCHEMA %1\$s TO %2\$s',
-      'GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s',
-      'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1\$s.slugs TO %2\$s'
+      'GRANT USAGE ON SCHEMA %1$s TO %2$s',
+      'GRANT SELECT ON TABLE %1$s.definitions TO %2$s',
+      'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1$s.slugs TO %2$s'
     ])->each(fn (string $statement) => DB::statement(sprintf($statement, Limax::getSchema(), $role)));
   }
 
