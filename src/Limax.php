182d3382953f69740b929d0033d632b1104219e7
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-09 16:54:22 +0000

    feat: add grant method

--- src/Limax.php
+++ src/Limax.php
@@ -2,9 +2,35 @@
 
 namespace Jaulz\Limax;
 
+use Illuminate\Support\Facades\DB;
+
 class Limax
 {
   public function getSchema() {
     return 'limax';
+  }
+
+  public function grant(string $role) {
+    DB::statement(
+      sprintf(
+        <<<PLPGSQL
+GRANT SELECT ON TABLE %1\$s.definitions TO %2\$s;
+PLPGSQL
+        ,
+        Limax::getSchema(),
+        $role
+      )
+    );
+    
+    DB::statement(
+      sprintf(
+        <<<PLPGSQL
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE %1\$s.slugs TO %2\$s;
+PLPGSQL
+        ,
+        Limax::getSchema(),
+        $role
+      )
+    );
   }
 }
