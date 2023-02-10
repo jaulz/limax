6407b729a9017e537178a5e9de059009d486bdc3
Julian Hundeloh <julian@hundeloh-consulting.ch>
2023-02-10 09:07:51 +0000

    feat: remember updated timestamp

--- src/Traits/IsSluggableTrait.php
+++ src/Traits/IsSluggableTrait.php
@@ -3,6 +3,7 @@
 namespace Jaulz\Limax\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
+use Tpetry\PostgresqlEnhanced\Query\Builder as PostgresBuilder;
 use Illuminate\Support\Facades\DB;
 use Jaulz\Limax\Facades\Limax;
 
@@ -35,47 +36,16 @@ trait IsSluggableTrait
    */
   public function scopeSlugged(Builder $query, string $value, string $column = 'slug', string $tableSchema = null)
   {
-    /*$query->where(function (Builder $query) use ($value) {
-      $query->where($this->getKeyName(), match ($this->getKeyType() === 'string') {
-        'string' => $value,
-        default => (int) $value,
-      });
-      $query->orWhere('slug', $value);
-    });*/
-
-    $definition = DB::table(Limax::getSchema() . '.definitions')->where('table_name', $this->getTable())
-      ->where('table_schema', $tableSchema ?? config('limax.table_schema'))
-      ->where('target_name', $column)->first();
-
-    if (!$definition) {
-      $query->where(DB::raw('1'), '=', DB::raw('0'));
-      return;
-    }
-
-    $slug = DB::table(Limax::getSchema() . '.slugs')->where('definition_id', $definition->id)
-      ->where('value', $value)->first();
-    if (!$slug) {
-      $query->where(DB::raw('1'), '=', DB::raw('0'));
-      return;
-    }
-
-    $query->where($this->getKeyName(), $slug->primary_key);
-
-    /*$slug = DB::table(Limax::getSchema() . '.slugs')
-    
-    table(Limax::getSchema() . '.definitions')->where('table_name', '=', DB::raw(sprintf("'%s'", $this->getTable())))
-    ->where('table_schema', 'public')
-    ->where('target_name', $column)
-
-    $query
-      ->join(Limax::getSchema() . '.definitions', function ($join) use ($column) {
-        $join->on('definitions.table_name', '=', DB::raw(sprintf("'%s'", $this->getTable())))
-          ->where('definitions.table_schema', 'public')
-          ->where('definitions.target_name', $column);
-      })
-      ->join(Limax::getSchema() . '.slugs', function ($join) use ($slug) {
-        $join->on('definitions.id', '=', 'slugs.definition_id')
-          ->where('slugs.value', '=', $slug);
-      });*/
+    $query->whereIn($this->getKeyName(),  function (PostgresBuilder $query) use ($value, $tableSchema, $column) {
+      $query->select(DB::raw(match ($this->getKeyType() === 'string') {
+        'string' => 'primary_key',
+        default => 'primary_key::int',
+      }))
+        ->from(Limax::getSchema() . '.slugs')
+        ->join(Limax::getSchema() . '.definitions', 'definitions.id', '=', 'slugs.definition_id')
+        ->where('definitions.table_schema', $tableSchema ?? config('limax.table_schema'))
+        ->where('definitions.target_name', $column)
+        ->where('slugs.value', $value);
+    });
   }
 }
